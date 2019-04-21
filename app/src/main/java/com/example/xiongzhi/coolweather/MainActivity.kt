2ae16fe66c9e08ef86344d00c6d653e0bebbc971
yangbowen <13627289588@163.com>
2019-04-21 21:12:34 +0800

    firsr commit

---
+++ app/src/main/java/com/example/xiongzhi/coolweather/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.xiongzhi.coolweather
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init();
+    }
+
+    private fun init() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    }
+}
